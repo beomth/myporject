782285aaa6a0d8d5546f846b14f51581da0850ad
beomth <xhxkf0202@naver.com>
2024-01-28 11:39:13 +0900

    Initial commit

---
+++ src/main/java/com/example/myporject/MyporjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myporject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyporjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyporjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myporject/MyporjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myporject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyporjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
